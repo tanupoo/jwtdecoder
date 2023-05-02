380362c9c0aec2eff4d5fa970f47c687d89c1004
tanupoo <github-shoichi@tanu.org>
2023-05-02 14:28:14 +0900

    updated

--- test.py
+++ test.py
@@ -1,29 +1,101 @@
 import jwtdecoder as jwd
 
-vecs = [
+# test vectors
+tv = [
+    """
+	eyJhbGciOiJSUzI1NiJ9
+	.
+	eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt
+	cGxlLmNvbS9pc19yb290Ijp0cnVlfQ
+	.
+	cC4hiUPoj9Eetdgtv3hF80EGrhuB__dzERat0XF9g2VtQgr9PJbu3XOiZj5RZmh7
+	AAuHIm4Bh-0Qc_lF5YKt_O8W2Fp5jujGbds9uJdbF9CUAr7t1dnZcAcQjbKBYNX4
+	BAynRFdiuB--f_nZLgrnbyTyWzO75vRK5h6xBArLIARNPvkSjtQBMHlb1L07Qe7K
+	0GarZRmB_eSN9383LcOLn6_dO--xi12jzDwusC-eOkHWEsqtFZESc6BfI7noOPqv
+	hJ1phCnvWh6IeYI2w9QOYEUipUTI8np6LbgGY9Fs98rqVt5AXLIhWkWywlVmtVrB
+	p0igcN_IoypGlUPQGe77Rw
+    """
+    ,
+	# 3.1. Example JWT
     """
     eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.dBjftJeZ4CVP-mB92K27uhbUJU1p1r_wW1gFWFOEjXk
     """
     ,
+	# 6.1.  Example Unsecured JWT
+    # Empty Signature
     """
-        eyJhbGciOiJSUzI1NiJ9
-        .
-        eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt
-        cGxlLmNvbS9pc19yb290Ijp0cnVlfQ
-        .
-        cC4hiUPoj9Eetdgtv3hF80EGrhuB__dzERat0XF9g2VtQgr9PJbu3XOiZj5RZmh7
-        AAuHIm4Bh-0Qc_lF5YKt_O8W2Fp5jujGbds9uJdbF9CUAr7t1dnZcAcQjbKBYNX4
-        BAynRFdiuB--f_nZLgrnbyTyWzO75vRK5h6xBArLIARNPvkSjtQBMHlb1L07Qe7K
-        0GarZRmB_eSN9383LcOLn6_dO--xi12jzDwusC-eOkHWEsqtFZESc6BfI7noOPqv
-        hJ1phCnvWh6IeYI2w9QOYEUipUTI8np6LbgGY9Fs98rqVt5AXLIhWkWywlVmtVrB
-        p0igcN_IoypGlUPQGe77Rw
+	eyJhbGciOiJub25lIn0
+    .
+    eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFt
+    cGxlLmNvbS9pc19yb290Ijp0cnVlfQ
+    .
     """
+    ,
+	# A.1.  Example Encrypted JWT
+	"""
+	eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2In0.
+	QR1Owv2ug2WyPBnbQrRARTeEk9kDO2w8qDcjiHnSJflSdv1iNqhWXaKH4MqAkQtM
+	oNfABIPJaZm0HaA415sv3aeuBWnD8J-Ui7Ah6cWafs3ZwwFKDFUUsWHSK-IPKxLG
+	TkND09XyjORj_CHAgOPJ-Sd8ONQRnJvWn_hXV1BNMHzUjPyYwEsRhDhzjAD26ima
+	sOTsgruobpYGoQcXUwFDn7moXPRfDE8-NoQX7N7ZYMmpUDkR-Cx9obNGwJQ3nM52
+	YCitxoQVPzjbl7WBuB7AohdBoZOdZ24WlN1lVIeh8v1K4krB8xgKvRU8kgFrEn_a
+	1rZgN5TiysnmzTROF869lQ.
+	AxY8DCtDaGlsbGljb3RoZQ.
+	MKOle7UQrG6nSxTLX6Mqwt0orbHvAKeWnDYvpIAeZ72deHxz3roJDXQyhxx0wKaM
+	HDjUEOKIwrtkHthpqEanSBNYHZgmNOV7sln1Eu9g3J8.
+	fiK51VwhsxJ-siBMR-YFiA
+	"""
+    ,
+	# A.2.  Example Nested JWT
+	"""
+     eyJhbGciOiJSU0ExXzUiLCJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiY3R5IjoiSldU
+     In0.
+     g_hEwksO1Ax8Qn7HoN-BVeBoa8FXe0kpyk_XdcSmxvcM5_P296JXXtoHISr_DD_M
+     qewaQSH4dZOQHoUgKLeFly-9RI11TG-_Ge1bZFazBPwKC5lJ6OLANLMd0QSL4fYE
+     b9ERe-epKYE3xb2jfY1AltHqBO-PM6j23Guj2yDKnFv6WO72tteVzm_2n17SBFvh
+     DuR9a2nHTE67pe0XGBUS_TK7ecA-iVq5COeVdJR4U4VZGGlxRGPLRHvolVLEHx6D
+     YyLpw30Ay9R6d68YCLi9FYTq3hIXPK_-dmPlOUlKvPr1GgJzRoeC9G5qCvdcHWsq
+     JGTO_z3Wfo5zsqwkxruxwA.
+     UmVkbW9uZCBXQSA5ODA1Mg.
+     VwHERHPvCNcHHpTjkoigx3_ExK0Qc71RMEParpatm0X_qpg-w8kozSjfNIPPXiTB
+     BLXR65CIPkFqz4l1Ae9w_uowKiwyi9acgVztAi-pSL8GQSXnaamh9kX1mdh3M_TT
+     -FZGQFQsFhu0Z72gJKGdfGE-OE7hS1zuBD5oEUfk0Dmb0VzWEzpxxiSSBbBAzP10
+     l56pPfAtrjEYw-7ygeMkwBl6Z_mLS6w6xUgKlvW6ULmkV-uLC4FUiyKECK4e3WZY
+     Kw1bpgIqGYsw2v_grHjszJZ-_I5uM-9RA8ycX9KqPRp9gc6pXmoU_-27ATs9XCvr
+     ZXUtK2902AUzqpeEUJYjWWxSNsS-r1TJ1I-FMJ4XyAiGrfmo9hQPcNBYxPz3GQb2
+     8Y5CLSQfNgKSGt0A4isp1hBUXBHAndgtcslt7ZoQJaKe_nNJgNliWtWpJ_ebuOpE
+     l8jdhehdccnRMIwAmU1n7SPkmhIl1HlSOpvcvDfhUN5wuqU955vOBvfkBOh5A11U
+     zBuo2WlgZ6hYi9-e3w29bR0C2-pp3jbqxEDw3iWaf2dc5b-LnR0FEYXvI_tYk5rd
+     _J9N0mg0tQ6RbpxNEMNoA9QWk5lgdPvbh9BaO195abQ.
+     AVO9iT5AV4CzvDJCdhSFlQ
+	"""
+	,
+	# Empty Signature, but ERROR
+    """
+    eyJ0eXAiOiJKV1QiLA0KICJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ.
+    """
+	,
 ]
 
-for v in vecs:
-    print(".".join(jwd.jwt_decode(v, hex_sig=True, verbose=True)))
+def test1_0():
+    print("\n".join(jwd.jwt_decode(tv[0], verbose=True)))
 
-for v in vecs:
-    for i in [0,1,2]:
-        print(i, jwd.jwt_decode(v, i, hex_sig=True, verbose=True)[0])
+def test1_1():
+    print("\n".join(jwd.jwt_decode(tv[1], verbose=True)))
 
+def test1_2():
+    print("\n".join(jwd.jwt_decode(tv[2], verbose=True)))
+
+def test1_3():
+    print("\n".join(jwd.jwt_decode(tv[3], verbose=True)))
+
+def test2_0():
+	print(0, jwd.jwt_decode(tv[0], 0, verbose=True)[0])
+
+#for v in vecs:
+#    for i in [0,1,2]:
+#        print(i, jwd.jwt_decode(v, i, verbose=True)[0])
+
+
+for i,v in enumerate(tv):
+	print(i, jwd.jwt_decode(tv[i], None, verbose=False))
